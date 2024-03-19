2ac3380eba05a8cc4c610c5d5fcfdb0fb8198116
AksLIntellipat <118931093+akslintellipat@users.noreply.github.com>
2024-03-19 14:01:37 +0530

    Create mainpage and navbar2

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -5,17 +5,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 
-function App() {
+const RedirectToHome = () => {
+  window.location.href = '/home.html';
+  return <></>;
+};
 
+function App() {
   return (
     <>
-        <Navbar/>
-        <Routes>
-          {/* the final code will not pass the products to every page, but each page will call the server API */}
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-        </Routes>
-        </>
+      <Navbar />
+      <Routes>
+        {/* the final code will not pass the products to every page, but each page will call the server API */}
+        <Route path="/" element={<MainPage />} />
+        <Route path="/app" element={<MainPage />} />
+        <Route path="/home.html" element={<RedirectToHome />} />
+      </Routes>
+    </>
   );
 }
 
