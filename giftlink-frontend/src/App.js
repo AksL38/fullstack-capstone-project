04058a24ff054cd4071031b1c414a23c7fd7beac
AksLIntellipat <118931093+akslintellipat@users.noreply.github.com>
2024-03-19 14:24:32 +0530

    Create Register and Login pages

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -4,6 +4,8 @@ import MainPage from './components/MainPage/MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
 
 const RedirectToHome = () => {
   window.location.href = '/home.html';
@@ -17,6 +19,8 @@ function App() {
       <Routes>
         {/* the final code will not pass the products to every page, but each page will call the server API */}
         <Route path="/" element={<MainPage />} />
+        <Route path="/app/login" element={<LoginPage />} />
+        <Route path="/app/register" element={<RegisterPage />} />
         <Route path="/app" element={<MainPage />} />
         <Route path="/home.html" element={<RedirectToHome />} />
       </Routes>
