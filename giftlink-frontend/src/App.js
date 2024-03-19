d0b2a20180d17f43ab6ca073012ada5a0047d72d
AksLIntellipat <118931093+akslintellipat@users.noreply.github.com>
2024-03-19 14:50:44 +0530

    Implement the search page

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -7,6 +7,7 @@ import Navbar from './components/Navbar/Navbar';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import DetailsPage from './components/DetailsPage/DetailsPage';
+import SearchPage from './components/SearchPage/SearchPage';
 
 const RedirectToHome = () => {
   window.location.href = '/home.html';
@@ -20,6 +21,7 @@ function App() {
       <Routes>
         {/* the final code will not pass the products to every page, but each page will call the server API */}
         <Route path="/" element={<MainPage />} />
+        <Route path="/app/search" element={<SearchPage />} />
         <Route path="/app/product/:productId" element={<DetailsPage />} />
         <Route path="/app/login" element={<LoginPage />} />
         <Route path="/app/register" element={<RegisterPage />} />
